ab1e64dd24c282613c9f49c7ec2fe534adea4847
wylanneely <wylanneely@gmail.com>
2017-10-02 00:02:02 -0700

    initial commit

---
+++ VisionFrameworkPlay/VisionFrameworkPlay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VisionFrameworkPlay
+//
+//  Created by ALIA M NEELY on 10/1/17.
+//  Copyright © 2017 Wylan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ VisionFrameworkPlay/VisionFrameworkPlay/VisionModel.swift
@@ -0,0 +1,25 @@
+//
+//  Vision.swift
+//  VisionFrameworkPlay
+//
+//  Created by ALIA M NEELY on 10/1/17.
+//  Copyright © 2017 Wylan. All rights reserved.
+//
+
+import Foundation
+import Vision
+
+
+class VisionModel {
+    
+    let image = 
+    
+    func getTextGromImage() {
+        
+        
+        
+    }
+
+    
+    
+}
